38b90bd09c130fee1290f7876ff2ae4b47cf96f7
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-10-17 21:32:49 +0200

    site based on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-var gulp = require('gulp')
+var gulp = require('gulp'),
+	htmlmin = require('gulp-htmlmin'),
+	sass = require('gulp-sass'),
+	webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
-	
+gulp.task('htmlmin', function() {
+	gulp.src('src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('.'))
 });
 
+gulp.task('sass', function() {
+	gulp.src('src/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('.'));
+});
+
+
+gulp.task('default',  ['sass', 'htmlmin']);
+gulp.task('serve', ['sass', 'htmlmin'], function() {
+	gulp.src('.').pipe(webserver());
+});
