941ddc52de616984ae5610a22250b15b59a6bd3d
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-11-25 07:11:11 +0100

    first website release

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
-var gulp = require('gulp'),
+var fs = require('fs'),
+	gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    template = require('gulp-template'),
 	webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
+gulp.task('render', function() {
+	_render('index', 'Koara - A Modular Lightweight Markup Language');
+	_render('grammar', 'Grammar - Koara');
+	_render('projects', 'Projects - Koara');
+	_render('demo', 'Demo - Koara');
+
+	//gulp.src('_template.html').pipe(rename('docs.html')).pipe(template({title: 'Documentation - Koara'})).pipe(gulp.dest('.'));
+	//gulp.src('_template.html').pipe(rename('grammar.html')).pipe(template({title: 'Grammar - Koara'})).pipe(gulp.dest('.'));
+	//gulp.src('_template.html').pipe(rename('projects.html')).pipe(template({title: 'Projects - Koara'})).pipe(gulp.dest('.'));
+	//gulp.src('_template.html').pipe(rename('demo.html')).pipe(template({title: 'Demo - Koara'})).pipe(gulp.dest('.'));
 });
 
-gulp.task('serve', function() {
+gulp.task('default', ['render']);
+gulp.task('serve', ['default'], function() {
+	gulp.watch('*.kd', ['render']);
 	gulp.src('.').pipe(webserver());
+
 });
+
+function _render(file, title) {
+	gulp.src('_template.html')
+		.pipe(rename(file + '.html'))
+		.pipe(template({title: title, content: fs.readFileSync(file + '.kd','utf8') }))
+		.pipe(gulp.dest('.'));
+}
