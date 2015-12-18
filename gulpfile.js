4a587815b2fd755dc96b7d67208469d375ec7c8c
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-12-18 20:19:19 +0100

    docs subdomain

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var fs = require('fs'),
 
 gulp.task('render', function() {
 	_render('index', 'Koara - A Modular Lightweight Markup Language');
-	_render('docs', 'Documentation - Koara');
+	_render('docs/index', 'Documentation - Koara');
 	_render('grammar', 'Grammar - Koara');
 	_render('projects', 'Projects - Koara');
 	_render('demo', 'Demo - Koara');
