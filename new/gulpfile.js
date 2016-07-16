34ef24777e20ab17e11713cf8ce15885a88be4fb
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-07-16 21:01:09 +0200

    add sass

--- new/gulpfile.js
+++ new/gulpfile.js
@@ -4,9 +4,16 @@ const gulp = require('gulp');
 gulp.task('html', function() {
 });
 
+gulp.task('sass', function () {
+    return gulp.src('src/base.scss')
+        .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
+        .pipe($.rename('a.css'))
+        .pipe(gulp.dest('.'));
+});
+
 gulp.task('serve', ['default'], function() {
 	gulp.watch('*.scss', ['sass']);
 	gulp.src('.').pipe($.webserver());
 });
 
-gulp.task('default', []);
+gulp.task('default', ['sass']);
