744f5a94debfbe18237e9f1ef8b0775db58eaed4
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-12-26 14:09:13 +0100

    Publish new website

---
+++ site.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var Handlebars = require('handlebars');
+var koara = require('koara');
+var koaraHtml = require('koara-html');
+
+var result = new koara.Parser().parse(fs.readFileSync('index.kd', "utf8"))
+var renderer = new koaraHtml.Html5Renderer();
+renderer.headingIds = true;
+result.accept(renderer);
+
+var template = Handlebars.compile(fs.readFileSync('layout.html', "utf8"));
+var html    = template({body: renderer.getOutput()});
+
+
+
+fs.writeFileSync("index.html", html);
